ddec204e58c0a529f0805deab37b61f57fdf8a86
deawick <Dhbns2!umjt!>
2017-02-15 16:31:28 +0100

    Überarbeitung LevelGenerator

--- src/de/tudarmstadt/informatik/fop/breakout/managers/LevelGenerator.java
+++ src/de/tudarmstadt/informatik/fop/breakout/managers/LevelGenerator.java
@@ -10,8 +10,9 @@ import de.tudarmstadt.informatik.fop.breakout.gameobjects.Block;
 
 public class LevelGenerator implements GameParameters {
 
-	private final static int BLOCKS_PER_ROW = 15;
-	private final static int SIDE_SPACE = ((WINDOW_WIDTH - (BLOCKS_PER_ROW * BLOCK_WIDTH)) / 2);
+	private final static int BLOCKS_PER_ROW = 16;
+	private final static int SIDE_SPACE = WINDOW_WIDTH / 2;
+	private final static int TOP_SPACE = WINDOW_HEIGHT / 2;
 
 	/**
 	 * loads a level from a '.map' file and returns an ArrayList<Block> with all
@@ -45,7 +46,7 @@ public class LevelGenerator implements GameParameters {
 				if (st.ttype == StreamTokenizer.TT_NUMBER) {
 
 					xPos = SIDE_SPACE + BLOCK_WIDTH * gridX;
-					yPos = BLOCK_HEIGHT / 2 + BLOCK_HEIGHT * gridY;
+					yPos = TOP_SPACE + BLOCK_HEIGHT * gridY;
 
 					switch ((int) st.nval) {
 					case 0:
