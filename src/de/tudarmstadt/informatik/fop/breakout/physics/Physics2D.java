1354191da6683d36a851c84e10a29ff84f5222ba
franpet <franpet@users.noreply.github.com>
2017-02-17 20:47:23 +0100

    updateAngleOffset: better calculation, comments extended, unused imports removed

--- src/de/tudarmstadt/informatik/fop/breakout/physics/Physics2D.java
+++ src/de/tudarmstadt/informatik/fop/breakout/physics/Physics2D.java
@@ -1,8 +1,6 @@
 package de.tudarmstadt.informatik.fop.breakout.physics;
 
-import org.newdawn.slick.geom.Vector2f;
 
-import de.tudarmstadt.informatik.fop.breakout.constants.GameParameters;
 import de.tudarmstadt.informatik.fop.breakout.gameobjects.Ball;
 import de.tudarmstadt.informatik.fop.breakout.gameobjects.Stick;
 
@@ -49,27 +47,27 @@ public class Physics2D {
 	 */
 	public static void updateAngleOffset(Ball b, Stick s) {
 		float diff = s.getPosition().getX() - b.getPosition().getX();
-		float o = 0;
-		if(Math.abs(diff) > (s.getSize().getX() / 2) + (b.getSize().getX() / 2)){	//detect if the ball hits the stick on the left or right border
-			b.setRotation(b.getRotation() -180);
+		float rotationOffset = 0;
+		if(Math.abs(diff) >= (s.getSize().getX() / 2) + (b.getSize().getX() / (2 * Math.sqrt(2)))){	//detect if the ball hit the stick on the left or right border
+			b.setRotation(b.getRotation() -180); //  send it in the opposite direction
 		}
 		else if (diff > 20) {
-			o = -(diff - 20) / 2.6f; // offset angle continuously gets bigger the
+			rotationOffset = -(diff - 20) / 2.6f; // offset angle continuously gets bigger the
 									// further from the center the stick is hit
 			
 		} else if (diff < -20) {
-			o = -(diff + 20) / 2.6f;
+			rotationOffset = -(diff + 20) / 2.6f;
 			
 		}
 		 // if the stick is hit in a range of 20px around the middle,
 					// don't offset the angle
 		
 		
-		float newRot = b.getRotation() + o;
+		float newRot = b.getRotation() + rotationOffset;
 		for(;newRot < 0; newRot += 360){ //Java thinks -90 % 360 = -90 and not 270
 			}
 		
-		if(newRot % 360 < 90 || newRot % 360 > 270){	//prevent the ball falling through the stick after adding offset in many hits
+		if(newRot % 360 < 90 || newRot % 360 > 270){	//prevent the ball falling through the stick due to rotation pointing downwards after adding offset multiple times
 		b.setRotation(newRot);
 		}
 		
