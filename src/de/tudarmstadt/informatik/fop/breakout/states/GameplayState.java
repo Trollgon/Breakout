53392b8433927c0972aecb77a408df0a091593d3
LukasL97 <lukas.lehmann97@gmail.com>
2017-02-23 12:18:48 +0100

    Fixed GameplayState one more time

--- src/de/tudarmstadt/informatik/fop/breakout/states/GameplayState.java
+++ src/de/tudarmstadt/informatik/fop/breakout/states/GameplayState.java
@@ -197,7 +197,8 @@ public class GameplayState implements GameParameters, GameState {
 		entityManager.addEntity(getID(), new BorderFactory(BorderType.LEFT).createEntity());
 		entityManager.addEntity(getID(), new BorderFactory(BorderType.TOP).createEntity());
 		entityManager.addEntity(getID(), new BorderFactory(BorderType.RIGHT).createEntity());
-		entityManager.addEntity(GAMEPLAY_STATE, stick);
+		stick = new Stick();
+		entityManager.addEntity(getID(), stick);
 		entityManager.addEntity(getID(), new Ball(stick));
 		entityManager.addEntity(getID(), new Lives());
 		entityManager.addEntity(getID(), new Score());
