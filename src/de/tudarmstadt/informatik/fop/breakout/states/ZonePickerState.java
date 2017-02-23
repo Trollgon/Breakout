158d1eb2cf69b236dcdc6014cb3865d832c8ac79
Trollgon <trollgon@ac-whispers.com>
2017-02-23 15:03:11 +0100

    Added ZoneState and ZonePicker

---
+++ src/de/tudarmstadt/informatik/fop/breakout/states/ZonePickerState.java
@@ -0,0 +1,213 @@
+package de.tudarmstadt.informatik.fop.breakout.states;
+
+import de.tudarmstadt.informatik.fop.breakout.constants.GameParameters;
+import de.tudarmstadt.informatik.fop.breakout.ui.Button;
+import eea.engine.entity.StateBasedEntityManager;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.GameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * @author Matthias Spoerkmann
+ */
+public class ZonePickerState implements GameParameters, GameState {
+
+    private int stateID;
+    StateBasedEntityManager entityManager;
+
+    /**
+     * Constructor of new campaign state.
+     * @param stateID of this state
+     */
+    public ZonePickerState() {
+        this.stateID = ZONE_PICKER_STATE;
+        entityManager = StateBasedEntityManager.getInstance();
+    }
+
+    @Override
+    public int getID() {
+        return this.stateID;
+    }
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+
+        g.drawImage(new Image(DEFAULT_MENU_IMAGE), 0, 0);
+
+        entityManager.addEntity(getID(), new Button(218, 190, ZONE_STATE, 1));
+
+        entityManager.renderEntities(container, game, g);
+
+        g.drawString("Zone 1", 110, 180);
+    }
+
+    @Override
+    public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+        entityManager.updateEntities(container, game, delta);
+    }
+
+    @Override
+    public void enter(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void leave(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void controllerLeftPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerLeftReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerRightPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerRightReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerUpPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerUpReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerDownPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerDownReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerButtonPressed(int i, int i1) {
+
+    }
+
+    @Override
+    public void controllerButtonReleased(int i, int i1) {
+
+    }
+
+    @Override
+    public void keyPressed(int i, char c) {
+
+    }
+
+    @Override
+    public void keyReleased(int i, char c) {
+
+    }
+
+    @Override
+    public void mouseWheelMoved(int i) {
+
+    }
+
+    @Override
+    public void mouseClicked(int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void mousePressed(int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void mouseReleased(int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void mouseMoved(int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void mouseDragged(int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void setInput(Input input) {
+
+    }
+
+    @Override
+    public boolean isAcceptingInput() {
+        return false;
+    }
+
+    @Override
+    public void inputEnded() {
+
+    }
+
+    @Override
+    public void inputStarted() {
+
+    }
+
+    @Override
+    public void pauseUpdate() {
+
+    }
+
+    @Override
+    public void pauseRender() {
+
+    }
+
+    @Override
+    public void unpauseUpdate() {
+
+    }
+
+    @Override
+    public void unpauseRender() {
+
+    }
+
+    @Override
+    public boolean isUpdatePaused() {
+        return false;
+    }
+
+    @Override
+    public boolean isRenderPaused() {
+        return false;
+    }
+
+    @Override
+    public void setUpdatePaused(boolean b) {
+
+    }
+
+    @Override
+    public void setRenderPaused(boolean b) {
+
+    }
+}
