875378a6268cba4eccf8c9d533e45f48cdd30257
deawick <Dhbns2!umjt!>
2017-03-23 14:10:49 +0100

    helllo

---
+++ src/de/tudarmstadt/informatik/fop/breakout/gameobjects/blocks/ObsidianBlock.java
@@ -0,0 +1,28 @@
+package de.tudarmstadt.informatik.fop.breakout.gameobjects.blocks;
+
+import org.newdawn.slick.SlickException;
+
+/**
+ * class to represent a snow block
+ * @author Jonas Henry Grebe
+ *
+ */
+public class ObsidianBlock extends AbstractBlock {
+
+	public ObsidianBlock(int xPos, int yPos) {
+		super(xPos, yPos);
+	}
+
+	@Override
+	void configureBlock() throws SlickException {
+		setHitSound(BLOCK_OBSIDIAN_HIT_SOUND);
+
+		setType(BlockGroup.SOLID);
+		setBlockImage(BLOCK_OBSIDIAN_IMAGE);
+
+		setHitsLeft(BLOCK_OBSIDIAN_HITSLEFT);
+		setScore(BLOCK_OBSIDIAN_SCORE);
+
+	}
+
+}
---
+++ src/de/tudarmstadt/informatik/fop/breakout/states/zonestates/MagmaZoneState.java
@@ -0,0 +1,86 @@
+package de.tudarmstadt.informatik.fop.breakout.states.zonestates;
+
+import de.tudarmstadt.informatik.fop.breakout.managers.CheckPointManager;
+import eea.engine.action.basicactions.ChangeStateInitAction;
+import eea.engine.entity.Entity;
+import eea.engine.event.basicevents.KeyPressedEvent;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import de.tudarmstadt.informatik.fop.breakout.constants.GameParameters;
+import de.tudarmstadt.informatik.fop.breakout.constants.StateParameters.ZoneType;
+import de.tudarmstadt.informatik.fop.breakout.ui.Breakout;
+import de.tudarmstadt.informatik.fop.breakout.ui.Button;
+import eea.engine.entity.StateBasedEntityManager;
+
+public class MagmaZoneState extends BasicGameState implements GameParameters {
+
+	private StateBasedEntityManager entityManager;
+
+	public MagmaZoneState() {
+		entityManager = StateBasedEntityManager.getInstance();
+	}
+
+	@Override
+	public void enter(GameContainer container, StateBasedGame game) throws SlickException {
+		super.enter(container, game);
+	}
+
+	@Override
+	public void init(GameContainer container, StateBasedGame game) throws SlickException {
+
+		if (!Breakout.getDebug()) {
+
+			entityManager.addEntity(getID(), new Button(208, 190, 301, ZoneType.MAGMAZONE));
+
+			if (CheckPointManager.getCheckpoint() > 301) {
+				entityManager.addEntity(getID(), new Button(208, 310, 302, ZoneType.MAGMAZONE));
+			}
+			if (CheckPointManager.getCheckpoint() > 302) {
+				entityManager.addEntity(getID(), new Button(598, 190, 303, ZoneType.MAGMAZONE));
+			}
+			if (CheckPointManager.getCheckpoint() > 303) {
+				entityManager.addEntity(getID(), new Button(598, 310, 304, ZoneType.MAGMAZONE));
+			}
+		}
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+
+		g.drawImage(new Image("images/backgrounds/background_magma.png"), 0, 0);
+
+		entityManager.renderEntities(container, game, g);
+
+		g.drawString("Level 1", 100, 180);
+
+		if (CheckPointManager.getCheckpoint() > 301) {
+			g.drawString("Level 2", 100, 300);
+		}
+
+		if (CheckPointManager.getCheckpoint() > 302) {
+			g.drawString("Level 3", 490, 180);
+		}
+		
+		if (CheckPointManager.getCheckpoint() > 303) {
+			g.drawString("Level 4", 490, 300);
+		}
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+		entityManager.updateEntities(container, game, delta);
+	}
+
+	@Override
+	public void leave(GameContainer container, StateBasedGame game) throws SlickException {
+		super.leave(container, game);
+	}
+
+	@Override
+	public int getID() {
+		return MAGMA_ZONE_STATE;
+	}
+
+}
